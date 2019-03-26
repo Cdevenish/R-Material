f11c8e7eb4dcffcbb158bec6b3bd2710f4eae833
Christian Devenish <Cdevenish@users.noreply.github.com>
2019-03-27 00:46:56 +0700

    Update makeTemplates.r

--- Functions/Audio/makeTemplates.r
+++ Functions/Audio/makeTemplates.r
@@ -5,7 +5,7 @@
 ## Function will match labels and sound files based on common filename, then extract templates for each 
 # labelled audio.
 
-makeTemplates <- function(path, tmptxt = "TEMPLATE", dens = 0.1, tz = "", labels = c("Audacity", "Raven"), ...){
+makeTemplates <- function(path, tmptxt="TEMPLATE", dens=0.1, tz="Asia/Jakarta", labels=c("Audacity", "Raven"), ...){
   
   library(tuneR) # to read waves in makeCorTemplate
   library(monitoR)
