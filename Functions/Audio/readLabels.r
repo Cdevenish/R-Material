41fc5f13885ef541d2888424219215c090bf4ed3
Christian Devenish <Cdevenish@users.noreply.github.com>
2020-01-22 14:21:40 +0000

    Update readLabels.r

--- Functions/Audio/readLabels.r
+++ Functions/Audio/readLabels.r
@@ -5,21 +5,23 @@ readLabels <- function(x, type = c("Audacity", "Raven")){
   # x are file paths to label text files
   # type is software used to create labels
   
-  
   type <- match.arg(type)
-  
-  labs <- lapply(x, read.table, header = F, sep = "\t", stringsAsFactors= F)
   
   labs.df <- switch(type, 
                     
-        Audacity = lapply(labs, function(x) {
+        Audacity = lapply(x, function(x) {
+          
+        labs <- read.table(x, header = F, sep = "\t", stringsAsFactors= F)
                       
-        e.ind <- seq(2,nrow(x),2)
-        o.ind <- seq(1,nrow(x),2)
+        e.ind <- seq(2,nrow(labs),2)
+        o.ind <- seq(1,nrow(labs),2)
                       
-        res <- cbind(x[o.ind,], x[e.ind, 2:3]) 
+        res <- cbind(labs[o.ind,], labs[e.ind, 2:3]) 
         colnames(res) <- c("start", "stop", "name", "minFreq", "maxFreq")
-        res <- data.frame(name = res$name, lapply(res[-3], as.numeric), stringsAsFactors = F)
+        res <- data.frame(id  = basename(x), 
+                          name = res$name, 
+                          lapply(res[-3], as.numeric), 
+                          stringsAsFactors = F)
         }),
         Raven = stop("Not implemented yet")
           )
