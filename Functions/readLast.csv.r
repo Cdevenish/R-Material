0dfb265b7fbe7a13f37f0757a23bf4b9f953324a
Christian Devenish <Cdevenish@users.noreply.github.com>
2020-03-18 10:52:33 +0000

    Update readLast.csv.r

--- Functions/readLast.csv.r
+++ Functions/readLast.csv.r
@@ -5,18 +5,27 @@ readLast.csv <- function(path, pattern, read = T, ...){
   
   # path, pattern, as in list.files. 
   # read whether to read csv and assign to output of function (TRUE), or return string of filename
-  # ... more args to read.csv
+  # ... more args to read.csv OR list.files
   
   #extras <- match.call(expand.dots=FALSE)$...
+  
   # make a list of arguments and then pass to do.call,,, 
-  #extras <- list(...)
-  #csv.call <- extras[which(names(extras) %in% formalArgs(list.files))]
+  extras <- list(...)
   
-  csv <- list.files(path, pattern = paste0(".*", pattern, ".*\\.csv$"), full.names = T)
+  list.files.call <- c(list(path = path, pattern = paste0(".*", pattern, ".*\\.csv$"), full.names = T),
+                       extras[which(names(extras) %in% formalArgs(list.files))])
+  csv <- do.call(list.files, list.files.call)
 
+  # get latest
   csv2 <- csv[which.max(file.mtime(csv))]
-  cat(paste0("Reading: ", "'",basename(csv2),"'"))
-  
-  if(read) read.csv(csv2, ...) else return(csv2)
 
+  cat(paste0("Reading: ", "'",basename(csv2),"'"))
+
+  if(read) {
+    # make csv call
+    csv.call <- c(list(file = csv2), extras[which(names(extras) %in% formalArgs(read.table))])
+    do.call(read.csv, csv.call)
+
+  } else return(csv2)
+  
 }
