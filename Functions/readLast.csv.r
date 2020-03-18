70fbb5407d8dfd39d1f80fe62b9b4df381eb269b
Christian Devenish <Cdevenish@users.noreply.github.com>
2020-03-18 10:39:49 +0000

    Update readLast.csv.r

--- Functions/readLast.csv.r
+++ Functions/readLast.csv.r
@@ -1,22 +1,22 @@
 
-# pattern <- "audio_locations"
-# path <- "C:/Users/55116479/Dropbox (Manchester Met)/R_online/JavaR/inData"
-
 ## reads latest version of a csv file in path (folder), identified with unique pattern
 
 readLast.csv <- function(path, pattern, read = T, ...){
   
   # path, pattern, as in list.files. 
   # read whether to read csv and assign to output of function (TRUE), or return string of filename
-  # ... more args to list.files or read.csv
+  # ... more args to read.csv
   
-  csv <- list.files(path, pattern = paste0(".*", pattern, ".*\\.csv$"), full.names = T, ...)
-  #csv2 <- csv[grepl(pattern, csv)] # or read all and get time for all.. 
+  #extras <- match.call(expand.dots=FALSE)$...
+  # make a list of arguments and then pass to do.call,,, 
+  #extras <- list(...)
+  #csv.call <- extras[which(names(extras) %in% formalArgs(list.files))]
+  
+  csv <- list.files(path, pattern = paste0(".*", pattern, ".*\\.csv$"), full.names = T)
+
   csv2 <- csv[which.max(file.mtime(csv))]
+  cat(paste0("Reading: ", "'",basename(csv2),"'"))
   
   if(read) read.csv(csv2, ...) else return(csv2)
-  cat(paste0("File read: ", "'",basename(csv2),"'"))
-  
+
 }
-
-
