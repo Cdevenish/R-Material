bb0e60b070858cc6986f6aaa8cfe58bdfb623e6c
Christian Devenish <Cdevenish@users.noreply.github.com>
2020-04-22 10:10:50 +0100

    Update wavCheck.r

--- Functions/Audio/wavCheck.r
+++ Functions/Audio/wavCheck.r
@@ -1,14 +1,21 @@
 
 # fn <- wavs.mtch
 # samp =44100; duration = 5; channels = 1; bits = 16
+# duration = NULL
+# samp = NULL
 
-wavCheck <- function(fn, samp =44100, duration = 5, channels = 1, bits = 16, rm = T){
+
+wavCheck <- function(fn, samp =44100, duration = 5, channels = 1, bits = 16, rm = F, fix){
   
   # fn are complete file paths for wav files
-  # samp is required sample rate
-  # duration is minimum duration required
+  # samp is required sample rate (or NULL not to test)
+  # duration is minimum duration required (or NULL not to test)
+  # Channels are the required number of channels (or NULL not to test)
+  # bits are the required bit depth of the wav (or NULL not to test)
   # rm. Logical , return vector of filepaths without those with duration < duration, and with 
   # sample rates not equal to samp
+  
+  # Check libraries av tuneR
   
   wav.info <- lapply(fn, tuneR::readWave, header = T)
   
@@ -19,20 +26,34 @@ wavCheck <- function(fn, samp =44100, duration = 5, channels = 1, bits = 16, rm 
   colnames(wav.m) <- c("sample.rate", "channels", "bits", "samples")
   wav.m <- cbind(wav.m, duration = wav.m[,4]/wav.m[,1])
   
-  head(wav.m)
+  # head(wav.m)
   
   # check sample rate
+  # If any of the criteria are NULL, then they are dropped from this matrix
   ind <- cbind(sample.rate = wav.m[,"sample.rate"] == samp,
                channels = wav.m[,"channels"] == channels,
                bits = wav.m[,"bits"] == bits,
                duration = wav.m[,"duration"] > duration)
   
-  head(ind)
+  # head(ind)
   rm.ind <- apply(ind, 1, all)
   
   print(paste(sum(!rm.ind), "files failed"))
-  print(nrow(ind) - colSums(ind))
+  
+  if(sum(!rm.ind)>0) print(nrow(ind) - colSums(ind))
         
-  if(rm) return(fn[rm.ind])
-
+  if(!missing(fix)) {
+    
+    if(!dir.exists(fix)) stop("fix must be a valid directory to store the reprocessed files")
+    
+    outFiles <- file.path(fix, basename(fn))
+    if(any(wav.m[,"sample.rate"] < samp)) warning("Sample rates lower than samp will be returned unchanged")
+    
+    mapply(function(x, y) av::av_audio_convert(x,y, channels = channels, sample_rate = samp, verbose = F),
+           fn, outFiles)
+    
+  }
+  
+  if(rm) return(fn[rm.ind]) else invisible(wav.m)
+  
 }
