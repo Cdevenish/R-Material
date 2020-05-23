02c35e43f30fabbd9e7870582f5d68439605f442
Christian Devenish <Cdevenish@users.noreply.github.com>
2020-05-23 14:07:58 +0100

    Update filter_pts.r

--- Functions/SDM/filter_pts.r
+++ Functions/SDM/filter_pts.r
@@ -1,7 +1,7 @@
 
 
 
-filter_pts <- function(r, pts, ext, crs, res, orig = c(0,0), writeID = F, gridID = "gridID"){
+filter_pts <- function(r, pts, byID, ext, crs, res, orig = c(0,0), writeID = F){
   
   library(raster)
   library(sf) # fix for converting within raster.. but should code all separately... 
@@ -9,62 +9,89 @@ filter_pts <- function(r, pts, ext, crs, res, orig = c(0,0), writeID = F, gridID
   # r is one of the prediction rasters (ie same extent, resolution, crs as all predictors), 
   ## if used, ignores ext, crs and res
   # pts is either sp or sf points spatial object
-  # writeID - logical. If TRUE, then returns unchanged points with a grid ID as new field. Default is false
+  # byID. Column name of group if points to be thinned by groups (eg species), otherwise all pts are used
+  # writeID - logical. If TRUE, then returns unchanged points with a gridID as new field. Default is false
   
   # make regular grid
-  if(missing(r)) r <- raster(ext, crs = crs, res = res) else r <- raster(r)
+  if(missing(r)) {
+    r <- raster(ext, crs = crs, res = res)
+    origin(r) <- orig
+    
+  } else r <- raster(r)
   
-  origin(r) <- orig
   
   ## add ID
-  #values(r) <- cellFromRowColCombine(r, 1:nrow(r), 1:ncol(r))
   values(r) <- 1:ncell(r) # add values - ID number for each cell
   names(r) <- "ID"
   #r
   
-  # extract id value to each occ point
-  grd.pts.id <- raster::extract(r, pts)
+  # group pts by byid and extract for each group
+  if(!missing(byID)){
+    
+    if(any(!is.character(byID), length(byID)!=1)) stop("byID must be a length one, character vector")
+    
+    f = pts[,byID] # isn't there a way to get a column from a sf object?
+    f$geometry <- NULL
+    f <- f[,,drop = T]
+    
+  } else f <- 1
   
-  # how many repeated values?
-  print(sprintf("%s grid cells have more than one record", sum(table(grd.pts.id)>1)))
+  pts.lst <- split(pts, f)
   
-  if(!writeID){
+  grd.pts <- lapply(pts.lst, function(x) raster::extract(r, x))
   
   ## keep first duplicated point, delete rest
-  dups <- duplicated(grd.pts.id)
+  dups <- lapply(grd.pts, duplicated)
+  # lengths(dups)
   
-  #sum(dups) # this is the total number of duplicated occurrence records (more than above as more 
-  # than 2 per cell duplicated in some cases)
+  # pts.lst[[1]][!dups[[1]],]
   
-  print(sprintf("%s records eliminated", sum(dups)))
-  
-  pts.filt <- pts[!dups,]
-  
-  return(pts.filt)
-  
-  } else {
+  if(writeID){
+    
+    # how many repeated values?
+    # print(sprintf("%s grid cells have more than one record", sum(table(grd.pts.id)>1)))
     
     ## check column name doesn't already exist... 
     
-    if(gridID %in% colnames(pts)){
-      
-      # ask if gridID should be overwritten or suggest new name
-      
-      # l <- length(colnames(pts))
-      # pts <- cbind(pts, grd.pts.id)
-      # colnames(pts)[l+1] <- paste0("gridID", paste(as.integer(runif(5, 0,9)), collapse = ""))
-      
-      ## for now.. warning
-      pts[, gridID] <- grd.pts.id
-      warning(gridID, " column is being overwritten")
-      
-      
-    } else pts[, gridID] <- grd.pts.id
+    # if(gridID %in% colnames(pts)){
     
-    return(pts)
+    pts.res <- mapply(function(x,y){
+      x <- cbind(x, gridID = y)
+    }, pts.lst, grd.pts, SIMPLIFY = F)
     
-  } 
+    pts.id <- do.call(rbind, pts.res)
+    
+    return(pts.id)
+    
+  } else {
+    
+    # ## keep first duplicated point, delete rest
+    # dups <- duplicated(grd.pts.id)
+    # 
+    #sum(dups) # this is the total number of duplicated occurrence records (more than above as more 
+    # than 2 per cell duplicated in some cases)
+    
+    print(sprintf("%s records eliminated in total", sum(unlist(dups))))
+    
+    if(!missing(byID)) {
+      print("By species:\n")
+      print(sapply(dups, sum))
+    }
+    # pts.filt <- pts[!dups,]
+    # 
+    # return(pts.filt)
+    
+    pts.res <- mapply(function(pts, dup) pts[!dup,], pts.lst, dups, SIMPLIFY = F)
+    
+    # bind together again
+    pts.filt <- do.call(rbind, pts.res)
+    rownames(pts.filt) <- NULL
+    return(pts.filt)
+    
+  }
+  
 }
+
 
 # 
 # ## Extract a random record for each raster cell (if repeated), per species
